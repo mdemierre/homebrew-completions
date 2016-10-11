95b72ce6e680296d1a5502533e83a038d323dea4
ilovezfs <ilovezfs@icloud.com>
2016-10-11 10:43:40 -0700

    brew-cask-completion 2.1
    Closes #144.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- brew-cask-completion.rb
+++ brew-cask-completion.rb
@@ -1,8 +1,8 @@
 class BrewCaskCompletion < Formula
   desc "Bash & Fish completion for brew-cask"
   homepage "https://github.com/xyb/homebrew-cask-completion"
-  url "https://github.com/xyb/homebrew-cask-completion/archive/v2.0.tar.gz"
-  sha256 "858d8b8ad5574a21220ac85025c7b36699dbb7188beca04be52596e73f34a68f"
+  url "https://github.com/xyb/homebrew-cask-completion/archive/v2.1.tar.gz"
+  sha256 "27c7ea3b7f7c060f5b5676a419220c4ce6ebf384237e859a61c346f61c8f7a1b"
   head "https://github.com/xyb/homebrew-cask-completion.git"
 
   bottle :unneeded
